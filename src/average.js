047237a908ddf9a0d91f43f1b4e29330a5fe97d7
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-01-18 19:44:17 -0300

    criada função avarege

--- src/average.js
+++ src/average.js
@@ -13,6 +13,17 @@
     
 */
 
-const average = () => {};
-
+const average = (array) => {
+  let somaDoArray = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    somaDoArray += array[index];
+  }
+  return Math.round(somaDoArray / array.length);
+};
 module.exports = average;
